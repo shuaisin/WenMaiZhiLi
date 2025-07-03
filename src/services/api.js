9b4019b29281db8524b6dd1fcab064de9210cd1f
shuaisin <shuaisin666888@163.com>
2025-07-04 01:38:04 +0800

    Create api.js

---
+++ src/services/api.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: window.location.origin,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+// 请求拦截器
+api.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('access_token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截器
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('access_token')
+      localStorage.removeItem('user_info')
+      window.location.href = '/auth'
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
